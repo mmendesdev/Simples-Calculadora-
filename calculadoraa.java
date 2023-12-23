1e294f89fa1ef483f65f9d0a1c1424e72c4ad6fb
Matheus Mendes <matheus.mendes@somosicev.com>
2023-12-23 01:06:46 -0300

    calculadora-simples

---
+++ calculadoraa.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+
+public class calculadoraa {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double num1, num2, resultado;
+        char operacao;
+
+        System.out.println("Digite o primeiro número:");
+        num1 = scanner.nextDouble();
+
+        System.out.println("Digite a operação (+, -, *, /):");
+        operacao = scanner.next().charAt(0);
+
+        System.out.println("Digite o segundo número:");
+        num2 = scanner.nextDouble();
+
+        switch (operacao) {
+            case '+':
+                resultado = num1 + num2;
+                System.out.println("Resultado: " + resultado);
+                break;
+            case '-':
+                resultado = num1 - num2;
+                System.out.println("Resultado: " + resultado);
+                break;
+            case '*':
+                resultado = num1 * num2;
+                System.out.println("Resultado: " + resultado);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    resultado = num1 / num2;
+                    System.out.println("Resultado: " + resultado);
+                } else {
+                    System.out.println("Erro: Divisão por zero!");
+                }
+                break;
+            default:
+                System.out.println("Operação inválida.");
+        }
+
+        
+        scanner.close();
+    }
+}
